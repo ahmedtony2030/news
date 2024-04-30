81fa98b8e1a107ee47dc862180abfba5d09ad504
ahmedtony2030 <168592073+ahmedtony2030@users.noreply.github.com>
2024-04-30 23:50:24 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+// let btn =document.getElementById('btn');
+//     let par = document.getElementsByClassName('center');
+//     btn.onclick=function(){
+//       par.classList.add("hide")
+//     }
